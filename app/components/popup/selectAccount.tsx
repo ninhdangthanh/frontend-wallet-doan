caf6826280794f99c6cb3f908b19154e3f1847e9
ninhnam <ninhnam027@gmail.com>
2024-03-10 18:06:07 +0700

    get coin of account with ethers.js

--- app/components/popup/selectAccount.tsx
+++ app/components/popup/selectAccount.tsx
@@ -4,10 +4,20 @@ import "../../../css/fontawesome-free-6.5.1-web/css/all.css"
 import "../../../css/bootstrap.min.css"
 import "../../../css/main.css"
 import "../../../css/popup.css"
+import { useSelector } from "react-redux";
+import { selectNetwork } from "@/redux/slice/networkSlice";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+
+
 
 export default function SelectAccountPopUp(props: any) {
     let { accounts } = props;
     
+    const network_redux = useSelector(selectNetwork);
+
+    const ethers_provider = new ethers.providers.JsonRpcProvider(network_redux.network?.rpc_url);
+
 
     return (
     <>
@@ -23,7 +33,7 @@ export default function SelectAccountPopUp(props: any) {
             <div className="network-select-body custom-overflow">
                 {
                     accounts.map((account: any, index: any) => {
-                        return <SelectAccountItem account={account} index={index} />
+                        return <SelectAccountItem network_redux={network_redux} ethers_provider={ethers_provider} account={account} index={index} />
                     })
                 }
             </div>
@@ -38,18 +48,36 @@ export default function SelectAccountPopUp(props: any) {
 
 
 function SelectAccountItem(props: any) {
-    const {account, index} = props
+    const {account, index, ethers_provider, network_redux} = props
+    const [coin, setCoin] = useState("0.000");
+
+    useEffect(() => {
+        getCoinOfAddress()
+    }, [])
+
+    const getCoinOfAddress = async () => {
+        try {
+            let coin = await ethers_provider.getBalance(account.address)
+            
+            let number = Number(`${coin}`)
+            let numberShow = (number / 1000000000000000000).toFixed(3)
+            
+            setCoin(numberShow)
+        } catch (error) {
+            setCoin("0.000")
+        }
+    }
     
     return (
         <>
             <div className="account-select-item">
                 <img src={`../account_list/${index + 1}.jpeg`} alt="N" className="network-select-item-logo"/>
                 <div className="account-select-item-name">
-                    <div className="network-select-item-name1">{account.name}</div>
+                    <div className="network-select-item-name1">{account.name.slice(0, 1).toUpperCase()}{account.name.slice(1, )}</div>
                     <div className="network-select-item-name2">{account.address.slice(0, 7)}...{account.address.slice(37, )}</div>
                 </div>
                 <div className="" style={{paddingRight: "50px"}}>
-                    <div className="network-select-item-name1">0.5 SepoliaETH</div>
+                    <div className="network-select-item-name1">{coin} {network_redux.network.currency_symbol}</div>
                 </div>
                 <div className="network-select-item-detail">
                     <i className="fa-solid fa-trash"></i>
