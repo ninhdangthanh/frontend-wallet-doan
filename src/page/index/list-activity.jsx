5570945c31b247b5fac8172564278cd61553d540
ninhdangthanh <ninhnam027@gmail.com>
2024-09-21 22:57:31 +0700

    update ui

--- src/page/index/list-activity.jsx
+++ src/page/index/list-activity.jsx
@@ -1,9 +1,11 @@
-export default function ListActivitys() {
+export default function ListActivitys(props) {
   return (
     <div className="flex flex-col h-screen items-start w-full p-4">
       <div className="flex-col flex items-start w-full">
         <p className="text-[18px] font-semibold">Jan 24 , 2024</p>
-        <div className="flex justify-between flex-row w-full p-2  mt-3 hover:bg-orange-900 hover:bg-opacity-25">
+        <div onClick={() => {
+                props.setPopupActivityDetail(true);
+              }} className="flex justify-between flex-row w-full p-3  mt-3 hover:bg-orange-900 hover:bg-opacity-25">
           <div className="flex justify-start items-center">
             <i className="fa-solid fa-arrow-up rotate-45 text-orangered p-2 border-2 border-orangered rounded-full w-9 h-9 "></i>
             <div className="flex-col items-start pl-3 flex">
@@ -11,12 +13,13 @@ export default function ListActivitys() {
               <p className="text-green">Confirm</p>
             </div>
           </div>
-          <div>
+          <div className="flex items-center">
             <p className="text-[18px] font-semibold">-0.12 ETH</p>
-            <p className="text-[16px] pt-1">-0.12 ETH</p>
           </div>
         </div>
-        <div className="flex justify-between flex-row w-full p-2   mt-3 hover:bg-orange-900 hover:bg-opacity-25">
+        <div onClick={() => {
+                props.setPopupActivityDetail(true);
+              }} className="flex justify-between flex-row w-full p-3   mt-3 hover:bg-orange-900 hover:bg-opacity-25">
           <div className="flex justify-start items-center">
             <i className="fa-solid fa-arrow-up rotate-45 text-orangered p-2 border-2 border-orangered rounded-full w-9 h-9 "></i>
             <div className="flex-col items-start pl-3 flex">
@@ -24,15 +27,16 @@ export default function ListActivitys() {
               <p className="text-red">Failed</p>
             </div>
           </div>
-          <div>
+          <div className="flex items-center">
             <p className="text-[18px] font-semibold">-0.12 ETH</p>
-            <p className="text-[16px] pt-1">-0.12 ETH</p>
           </div>
         </div>
       </div>
       <div className="flex-col flex items-start pt-5 w-full">
         <p className="text-[18px] font-semibold">Jan 24 , 2024</p>
-        <div className="flex justify-between flex-row w-full p-2  mt-3 hover:bg-orange-900 hover:bg-opacity-25">
+        <div onClick={() => {
+                props.setPopupActivityDetail(true);
+              }} className="flex justify-between flex-row w-full p-3  mt-3 hover:bg-orange-900 hover:bg-opacity-25">
           <div className="flex justify-start items-center">
             <i className="fa-solid fa-arrow-up rotate-[225deg] text-orangered p-2 border-2 border-orangered rounded-full w-9 h-9 "></i>
             <div className="flex-col items-start pl-3 flex">
@@ -40,12 +44,13 @@ export default function ListActivitys() {
               <p className="text-green">Confirm</p>
             </div>
           </div>
-          <div>
+          <div className="flex items-center">
             <p className="text-[18px] font-semibold">-0.12 ETH</p>
-            <p className="text-[16px] pt-1">-0.12 ETH</p>
           </div>
         </div>
-        <div className="flex justify-between flex-row w-full p-2   mt-3 hover:bg-orange-900 hover:bg-opacity-25">
+        <div onClick={() => {
+                props.setPopupActivityDetail(true);
+              }} className="flex justify-between flex-row w-full p-3   mt-3 hover:bg-orange-900 hover:bg-opacity-25">
           <div className="flex justify-start items-center">
             <i className="fa-solid fa-arrow-up rotate-45 text-orangered p-2 border-2 border-orangered rounded-full w-9 h-9 "></i>
             <div className="flex-col items-start pl-3 flex">
@@ -53,9 +58,8 @@ export default function ListActivitys() {
               <p className="text-yellow">Pending</p>
             </div>
           </div>
-          <div>
+          <div className="flex items-center">
             <p className="text-[18px] font-semibold">-0.12 ETH</p>
-            <p className="text-[16px] pt-1">-0.12 ETH</p>
           </div>
         </div>
       </div>
